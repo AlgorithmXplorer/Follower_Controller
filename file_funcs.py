a6aff9daea9ce5d814072d0a1f434b90351d448c
AlgorithmXplorer <tanrininkirbaci36@gmail.com>
2025-11-03 16:55:35 +0300

    the file funcs that owns FileNotFoundError error have been fixed

--- file_funcs.py
+++ file_funcs.py
@@ -1,33 +1,44 @@
-import os 
+import time
 import json
 
 def user_datas() -> dict:
     """
     Return gmail app_pasw gml_pasw and userame 
     """
-    with open("datas/datas.json","r+",encoding="utf-8") as file:
-        return json.load(file)
+    for i in range(10):
+        try:
+            with open("datas/datas.json","r+",encoding="utf-8") as file:
+                return json.load(file)
+        except:
+            time.sleep(5)
+        else:
+            break
+
 
 def user_saving(follows:dict = None,followers:dict = None) -> dict:
     """
     Return follower and follows or saving them 
     """
-    if followers == None and follows == None:
-        with open("datas/users.json","r+",encoding="utf-8") as file:
-            datas:dict = json.load(file)
-        return datas
+    for i in range(10):
+        try:    
+            if followers is None and follows is None:
+                with open("datas/users.json","r+",encoding="utf-8") as file:
+                    datas:dict = json.load(file)
+                return datas
 
-    with open("datas/users.json","r+", encoding="utf-8") as file:
-        datas:dict = json.load(file)
+            with open("datas/users.json","r+", encoding="utf-8") as file:
+                datas:dict = json.load(file)
 
-    if followers == None and follows != None:
-        with open("datas/users.json","w", encoding="utf-8") as file:
-            datas["follows"] = follows
-            json.dump(datas,file,sort_keys=False,indent=4)
-    
-    elif follows == None and followers != None:
-        with open("datas/users.json","w", encoding="utf-8") as file:
-            datas["followers"] = followers
-            json.dump(datas,file,sort_keys=False,indent=4)
-    
+            if followers is None and follows is not None:
+                with open("datas/users.json","w", encoding="utf-8") as file:
+                    datas["follows"] = follows
+                    json.dump(datas,file,sort_keys=False,indent=4)
 
+            elif follows is None and followers is not None:
+                with open("datas/users.json","w", encoding="utf-8") as file:
+                    datas["followers"] = followers
+                    json.dump(datas,file,sort_keys=False,indent=4)
+        except:
+            time.sleep(5)
+        else:
+            break
