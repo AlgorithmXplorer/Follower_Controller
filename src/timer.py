e65a3f19dc4dc3018684da76a60801f1932875ff
AlgorithmXplorer <tanrininkirbaci36@gmail.com>
2025-11-14 23:48:51 +0300

    description of the time function has been written

--- src/timer.py
+++ src/timer.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from datetime import timedelta
 
-
+# The purpose of this function is to ensure that the bot runs at the start of every hour.
+# To do this, it calculates how many seconds are left until the next hour and outputs it.
 def timer() -> int:
     now = datetime.now()
     one_hour_later = now + timedelta(hours=1)
