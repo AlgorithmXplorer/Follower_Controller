ff4c3f951baa335f04993ee7ea3bec500eed6ebc
AlgorithmXplorer <tanrininkirbaci36@gmail.com>
2025-11-15 00:44:39 +0300

    The descriptions of file functions have been written.

--- src/file_funcs.py
+++ src/file_funcs.py
@@ -3,10 +3,18 @@ import json
 
 path = "the path of folder that contains data json files"
 
+
+# This function reads and returns the user information used across methods and functions
+# from the "datas.json" file.
+# If it fails to fetch the necessary data, the bot cannot operate. For instance, without the username
+# or password, the login process cannot proceed and the program will crash.
 def user_datas() -> dict:
     """
     Return gmail app_pasw gml_pasw and userame 
     """
+    # A loop is used so that it doesn’t throw an immediate error if it cannot access
+    # the data files (the JSON files).
+    # If you start the application without adding the data files, you will have some time to fix it.
     for i in range(10):
         try:
             with open(path + "/datas.json","r+",encoding="utf-8") as file:
@@ -17,13 +25,17 @@ def user_datas() -> dict:
             return data
             
 
-
+    # The purpose of this function is to ensure that old follower and following data
+    # is updated during the hourly checks.
 def user_saving(follows:dict = None,followers:dict = None) -> dict:
     """
     Return follower and follows or saving them 
     """
 
     if followers is None and follows is None:
+        # A loop is used so that it doesn’t throw an immediate error if it cannot access
+        # the data files (the JSON files).
+        # If you start the application without adding the data files, you will have some time to fix it.
         for i in range(10):
             try:
                 with open(path + "/users.json","r+",encoding="utf-8") as file:
@@ -33,6 +45,9 @@ def user_saving(follows:dict = None,followers:dict = None) -> dict:
                 time.sleep(5)
 
 
+        # A loop is used so that it doesn’t throw an immediate error if it cannot access
+        # the data files (the JSON files).
+        # If you start the application without adding the data files, you will have some time to fix it.
     for i in range(10):
         try:    
             with open(path + "/users.json","r+", encoding="utf-8") as file:
