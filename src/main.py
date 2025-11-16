7aa85e42043bb1a5a95c8fc88736fff358dbd71d
AlgorithmXplorer <tanrininkirbaci36@gmail.com>
2025-11-17 00:52:51 +0300

    The descriptions of the main file codes have been completed.

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ class Main:
         self.bot.log_in(mail_reader=mail_reader)
         time.sleep(2)        
 
-
+    # Infinite loop method that ensures operations run at the start of every hour
     def main(self):
         time.sleep(30)
         while True:
@@ -29,38 +29,52 @@ class Main:
             time.sleep(wait_time)
 
 
-
+    # This method contains all follower-related operations. It combines them all together
     def follower_cont(self):
-        there_are_followers = user_saving()["followers"] 
-        if there_are_followers == {}:
+    
+        # First, it retrieves the follower data from the saved file 
+        are_there_followers = user_saving()["followers"] 
+
+        # If no data exists, the bot will first save follower data. This is for the first run or when data is deleted.
+        if are_there_followers == {}:
             user_saving(followers=self.bot.follower_taker()) 
         
+        # Retrieve the old list of followers. This is needed to compare with the new list.
         old_followers = user_saving()["followers"]
-
         old_names = list(old_followers.keys()) 
         
+        # Retrieve current followers from the profile using the bot and convert to DataFrame
         new_follower_list = self.bot.follower_taker()
         new_dict = {"names":list(new_follower_list.keys()) , "links":list(new_follower_list.values())}
         new_df = pd.DataFrame(data=new_dict) 
         
+        # Using apply(), get users who are in the new list but not in the old list → these are new followers.
         new_followers = new_df[new_df["names"].apply(func= lambda str_data: str_data not in old_names)]
+        
+        # If there are new followers, update the list and send them via email as a message.
         if len(new_followers.index) != 0:
             msg = self.bot.mail_information(new_followers)
 
             Mailer(app_psw=self.datas["app_password"],gmail=self.datas["gmail"],datas=msg)
             user_saving(followers=new_follower_list)
 
+
+    # This method contains all follow-related operations. It combines them all together
     def follow_cont(self):
+
+        # Retrieve the following list and save it because it will be compared with the follower list
         follows_list = self.bot.following_taker()
         user_saving(follows=follows_list)
 
+        # Convert the following list into a DataFrame
         follows_dict = {"names":list(follows_list.keys()), "links":list(follows_list.values())} 
         df_follow = pd.DataFrame(data=follows_dict)
 
+        # Compare following list with follower list to find users who do not follow you back
         follower_list = list(user_saving()["followers"].keys())
-
         unfollowers = df_follow[df_follow["names"].apply(func=lambda name: name not in follower_list)]
         
+        # Pass non-followers through the profile filter and unfollow unnecessary accounts
         if len(unfollowers.index) != 0:
             after_fiter = self.bot.profile_filter(users=unfollowers)
             will_unflw = after_fiter[0]
